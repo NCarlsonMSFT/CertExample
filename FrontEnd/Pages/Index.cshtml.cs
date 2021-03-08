f4bc440803973aac6fbac677a426d2289698b1e3
Nathan Carlson <ncarlson@microsoft.com>
2021-03-08 10:25:17 -0800

    Add project files.

---
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using BackEnd;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5002");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "Front End" });
+            ViewData["Message"] = reply.Message;
+        }
+    }
+}
